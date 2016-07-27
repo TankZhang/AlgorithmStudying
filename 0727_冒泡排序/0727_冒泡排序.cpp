d62cbb75cb833e4a121ce6501cddb3f79e5b86a2
Tank Zhang <tankzhang9712@outlook.com>
2016-07-27 17:02:19 +0800

    加入插入排序

--- 0727_冒泡排序/0727_冒泡排序.cpp
+++ 0727_冒泡排序/0727_冒泡排序.cpp
@@ -3,6 +3,16 @@
 
 #include "stdafx.h"
 
+/*冒泡排序循环n次将当前的值到i，立flag为true，
+循环到i时候，首先将flag置作false，然后从j=1到n-i，判断nums[j-1]和nums[j]，如果前者大则交换并置flag为true，交换（n-i）次。
+当没有交换，也就是flag一直为false时候已经排好序，外循环停止*/
+
+/*时间复杂度。平均情况：O(n^2)。最好情况：O(n)。最差情况：O(n^2)。辅助空间：O(1)。稳定性：稳定。*/
+
+/*时间复杂度分析。其外层循环执行 N - 1次。内层循环最多的时候执行N-i次，最少的时候执行1次，平均执行 (N+1)/2次。
+所以循环体内的比较交换约执行 (N - 1)(N + 1) / 2 = (N^2 - 1)/2（其中N^2是仿照Latex中的记法，表示N的平方）。
+按照计算复杂度的原则，去掉常数，去掉最高项系数，其复杂度为O(N^2)。*/
+
 //冒泡1
 void BubbleSort1(int a[], int n)
 {
@@ -77,11 +87,31 @@ void BubbleSort2_1(int nums[], int n)
 	}
 }
 
+//2nd time
+void BubbleSort2_2(int nums[], int n)
+{
+	bool flag = true;
+	for (int i = 0; i < n&&flag; i++)
+	{
+		flag = false;
+		for (int j = 1; j < n-i; j++)
+		{
+			if (nums[j - 1] > nums[j])
+			{
+				flag = true;
+				nums[j - 1] = nums[j - 1] + nums[j];
+				nums[j] = nums[j - 1] - nums[j];
+				nums[j - 1] = nums[j - 1] - nums[j];
+			}
+		}
+	}
+}
+
 int main()
 {
 	int nums[] = { 1,3,2,7,8,5,4 };
 	int n = 7;
-	BubbleSort2_1(nums, n);
+	BubbleSort2_2(nums, n);
 	for (int i = 0; i < n; i++)
 	{
 		printf("%d  ", nums[i]);
---
+++ 0727_直接插入排序/0727_直接插入排序.cpp
@@ -0,0 +1,106 @@
+// 0727_直接插入排序.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+
+
+/*直接插入排序，为从i=1开始，找前面某个值小于它并且不是紧邻的，如果找到，则从此数到i数中间的所有数字都要移动到i之后*/
+
+/*平均情况O(n^2)，最坏情况O(n^2),最好情况O(n)，辅助存储O(1)，稳定性：稳定*/
+
+/*最坏情况下，数组完全逆序，插入第2个元素时要考察前1个元素，插入第3个元素时，要考虑前2个元素，……，
+插入第N个元素，要考虑前 N - 1 个元素。因此，最坏情况下的比较次数是 1 + 2 + 3 + ... + (N - 1)，等差数列求和，结果为 N^2 / 2
+所以最坏情况下的复杂度为 O(N^2)。最好情况下，数组已经是有序的，每插入一个元素，只需要考查前一个元素，因此最好情况下，插入排序的时间复杂度为O(N)。*/
+
+
+//直接插入排序1
+void InsertionSort(int nums[], int n)
+{
+	int i, j, k, temp;
+	for (i = 1; i < n; i++)
+	{
+		for (j = i - 1; j >= 0; j--)
+			if (nums[j] < nums[i])
+				break;
+
+		if (j != i - 1)
+		{
+			temp = nums[i];
+			for (k = i - 1; k > j; k--)
+				nums[k + 1] = nums[k];
+			nums[k + 1] = temp;
+		}
+	}
+}
+
+//第二遍写
+void InsertionSort2(int nums[], int n)
+{
+	int i, j, k, temp;
+	for (i = 1; i < n; i++)
+	{
+		for (j = i - 1; j >= 0; j--)
+			if (nums[j] < nums[i])
+				break;
+		if (j != i - 1)
+		{
+			temp = nums[i];
+			for (k = i - 1; k > j; k--)
+				nums[k + 1] = nums[k];
+			nums[k + 1] = temp;
+		}
+	}
+}
+
+//3rd times
+void InsertionSort3(int nums[], int n)
+{
+	int i, j, k, temp;
+	for (i = 1; i < n; i++)
+	{
+		for (j = i - 1; j >= 0; j--)
+			if (nums[j] < nums[i])
+				break;
+		if (j != i - 1)
+		{
+			temp = nums[i];
+			for (k = i - 1; k > j; k--)
+				nums[k + 1] = nums[k];
+			nums[k + 1] = temp;
+		}
+	}
+}
+
+//4th times
+void InsertionSort4(int nums[], int n)
+{
+	int i, j, k, temp;
+	for (i = 1; i < n; i++)
+	{
+		for (j = i - 1; j >= 0; j--)
+			if (nums[j] < nums[i])
+				break;
+		if (j != i - 1)
+		{
+			temp = nums[i];
+			for (k = i - 1; k > j; k--)
+				nums[k + 1] = nums[k];
+			nums[k + 1] = temp;
+		}
+	}
+}
+
+int main()
+{
+	int nums[] = { 1,3,2,7,8,5,4 };
+	//int nums[] = {7,6,5,4,3,2,1 };
+	int n = 7;
+	InsertionSort4(nums, n);
+	for (int i = 0; i < n; i++)
+	{
+		printf("%d  ", nums[i]);
+	}
+	getchar();
+	return 0;
+}
+
