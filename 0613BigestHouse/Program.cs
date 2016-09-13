27cb1dcce5e6e661cdb3420f6a40b80b7d5d6498
Tank Zhang <tankzhang9712@outlook.com>
2016-09-13 21:13:33 +0800

    更新房子求法

--- 0613BigestHouse/Program.cs
+++ 0613BigestHouse/Program.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 namespace _0613BigestHouse
 {
     //得到最大的房子面积,连续的1为房顶，只有上下左右为连续。否则不为一个房子 。
+    //更新，改成求所有房子的数目
     class Program
     {
         static void Main(string[] args)
@@ -18,13 +19,13 @@ namespace _0613BigestHouse
 
         static void GetBigMain()
         {
-            int[,] grid = { { 0, 0, 0, 0 }, { 0, 1, 0, 0 }, { 0, 0, 1, 0 }, { 0, 0, 0, 0 } };
+            int[,] grid = { { 0, 0, 0, 0 }, { 0, 1,1, 0 }, { 0, 1, 1, 0 }, { 1, 0, 0, 0 } };
             Console.WriteLine(GetBig(grid));
         }
 
         //初始化一个最大面积的int类型。
         static int max = 0;
-        static int maxTemp = 0;
+        static int count = 0;
 
         //设置变量保存行数目和列数目
         static int row = 0;
@@ -48,28 +49,27 @@ namespace _0613BigestHouse
             //获取行列
             row = flag.GetLength(0);
             column = flag.GetLength(1);
+            count = 0;
             for (int i = 0; i < row; i++)
             {
                 for (int j = 0; j < column; j++)
                 {
                     if (grid[i, j] == 0 || flag[i, j] == 1)
                         continue;
-                    //初始化临时最大面积为0
-                    maxTemp = 0;
+                    //初始化房子数量为0
                     //初始化到来方向
                     int direc = 1;
                     while (Check(grid, flag, i, j, direc) != 0)
                     { }
-                    max = max > maxTemp ? max : maxTemp;
+                    count++;
                 }
             }
-            return max;
+            return count;
         }
 
         //处理所有与当前方块连续的不为0的数据,direc为来的方向，左上右下
         private static int Check(int[,] grid, int[,] flag, int i, int j, int direc)
         {
-            maxTemp++;
             flag[i, j] = 1;
             if (direc != 1 && j > 0 && grid[i, j - 1] == 1 && flag[i, j - 1] == 0)
                 Check(grid, flag, i, j - 1, 3);
